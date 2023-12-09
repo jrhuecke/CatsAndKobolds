7c1d5e834611c49634701387ac463d520ee6c318
Vulon <83103374+VulonDev@users.noreply.github.com>
2023-12-08 22:00:52 -0800

    not sure why it wont build

---
+++ Source/CatsAndKobolds/Public/PotionDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PotionDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CATSANDKOBOLDS_API UPotionDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
